4ba01abf695854687f86ff94beba5e2db070c325
CdeJohane <j.r.t.johane@gmail.com>
2024-09-08 17:28:13 +0200

    Styling of Standings

--- lib/pages/standings.dart
+++ lib/pages/standings.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:soccer_predict_admin/controller/player_request.dart';
 
 class StandingsPage extends StatelessWidget {
@@ -22,26 +24,46 @@ class StandingsPage extends StatelessWidget {
             return ListView.builder(
               itemCount: length,
               itemBuilder: (context, index) {
-                return SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  height: 60,
-                  child: Padding(
-                    padding: const EdgeInsets.only(left:18.0),
-                    child: Row(
-                      children: [
-                        Expanded(
-                          flex: 1,
-                          child: Text('${index+1}'),
-                        ),
-                        Expanded(
-                          flex: 5,
-                          child: Text(snapshot.data?[index]['player_name'])
-                        ),
-                        Expanded(
-                          flex: 1,
-                          child: Text('${snapshot.data?[index]['points']}')
+                return GestureDetector(
+                  onTap: () {
+                    String myText = snapshot.data?[index]['player_name'];
+                    Fluttertoast.showToast(
+                      msg: "$myText has been pressed",
+                      toastLength: Toast.LENGTH_SHORT,
+                      backgroundColor: Colors.grey[700],
+                      textColor: Colors.white70
+                    );
+                  },
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: 60,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(
+                            color: Colors.grey[200]!
+                          )
                         )
-                      ]
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left:18.0),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              flex: 1,
+                              child: Text('${index+1}', style: TextStyle(fontSize: 16, fontWeight: FontWeight.w200),),
+                            ),
+                            Expanded(
+                              flex: 5,
+                              child: Text(snapshot.data?[index]['player_name'], style: TextStyle(fontSize: 16, fontWeight: FontWeight.w800),)
+                            ),
+                            Expanded(
+                              flex: 1,
+                              child: Text('${snapshot.data?[index]['points']}', style: TextStyle(fontSize: 16, fontWeight: FontWeight.w800),)
+                            )
+                          ]
+                        ),
+                      ),
                     ),
                   ),
                 );
