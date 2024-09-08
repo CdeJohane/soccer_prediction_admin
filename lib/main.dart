47699a146d1aea88504282510de1ad3f19a4d66f
CdeJohane <j.r.t.johane@gmail.com>
2024-09-08 15:17:29 +0200

    Controller code to add all players

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:soccer_predict_admin/pages/main_screen.dart';
 
 
-void main() {
+void main() async{
+  await dotenv.load(fileName: '.env');
   runApp(
     const ProviderScope(
       child: MaterialApp(
