9aeaaeeaa03b5de5e21734f20c4dad1b783fe469
CdeJohane <j.r.t.johane@gmail.com>
2024-09-08 12:52:02 +0200

    Added stl for pages

---
+++ lib/pages/fixtures.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FixturesPage extends StatelessWidget {
+  const FixturesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
