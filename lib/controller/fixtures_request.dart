b068e6dd3cf02188401905cb4f6ff1189730c3b9
CdeJohane <j.r.t.johane@gmail.com>
2024-09-09 20:07:01 +0200

    Show Fixtures in Fixture Page

--- lib/controller/fixtures_request.dart
+++ lib/controller/fixtures_request.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 
-Future<List<dynamic>> getAllPlayers() async{
+Future<List<dynamic>> getAllFixtures() async{
   var result = await http.get(Uri.parse(dotenv.env['BACKEND_SITE_GET_FIXTURES']!));
   var resultArray = jsonDecode(result.body);
   return resultArray;
--- lib/pages/fixtures.dart
+++ lib/pages/fixtures.dart
@@ -1,12 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:soccer_predict_admin/controller/fixtures_request.dart';
 
 class FixturesPage extends StatelessWidget {
   const FixturesPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-        child: Text('Hello From Fixtures'),
+    return Center(
+      child: FutureBuilder(
+        future: getAllFixtures(), 
+        builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
+          // Check the state of the Future
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Text('Loading');  // Show loading indicator while waiting
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');  // Show error if any
+          } else if (snapshot.hasData) {
+            // Access the List<dynamic> once data is available
+            int length = snapshot.data?.length ?? 0;  // Get the length of the list
+            return ListView.builder(
+              itemCount: length,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    Fluttertoast.showToast(
+                      msg: "I have been Pressed",
+                    );
+                  },
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: 60,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(
+                            color: Colors.grey[200]!
+                          )
+                        )
+                      ),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            flex: 3,
+                            child: Align(alignment: Alignment.center ,child:Text('${snapshot.data?[index]['home_team_code']}', style: TextStyle(fontSize: 16, fontWeight: FontWeight.w800),)),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Align(alignment: Alignment.center, child: Text('-', style: TextStyle(fontSize: 14, fontWeight: FontWeight.w800),))
+                          ),
+                          Expanded(
+                            flex: 3,
+                            child: Align(alignment:Alignment.center, child: Text('${snapshot.data?[index]['away_team_code']}', style: TextStyle(fontSize: 16, fontWeight: FontWeight.w800),))
+                          )
+                        ]
+                      ),
+                    ),
+                  ),
+                );
+              },
+            );
+          } else {
+            return const Text('No data available');
+          }
+        },
+      ),
     );
   }
 }
