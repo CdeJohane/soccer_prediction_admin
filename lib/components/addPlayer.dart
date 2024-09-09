82daaf9f6421fd42d4c69b61fb9c9e150fc22b64
CdeJohane <j.r.t.johane@gmail.com>
2024-09-09 18:55:52 +0200

    HTTP post backend add player

--- lib/components/addPlayer.dart
+++ lib/components/addPlayer.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 class AddPlayerBtn extends StatelessWidget {
   const AddPlayerBtn({super.key});
 
@@ -19,10 +21,30 @@ class AddPlayerBtn extends StatelessWidget {
                 ),
           actions: [
             TextButton(
-              onPressed: (){
+              onPressed: () async{
                 String name = nameController.text;
-                Fluttertoast.showToast(msg: "$name");
-                Navigator.pop(context);
+                // Name has to be >3 Characters
+                if(name.length<=3){
+                  Fluttertoast.showToast(msg: "Enter Correct Length. More than 3 Characters");
+                }else{
+                  final Map<String, String> data = {
+                    'name': name,
+                  };
+                  // Send Details to database for verification and addition
+                  var httpResponse = await http.post(
+                    Uri.parse(dotenv.env['BACKEND_SITE_ADD_PLAYER']!),
+                    headers: {
+                      'Content-Type': 'application/json',
+                    },
+                    body: jsonEncode(data)
+                  );
+                  if (httpResponse.statusCode == 200){
+                    Fluttertoast.showToast(msg: "$name has been added");
+                    Navigator.pop(context);
+                  } else {
+                    Fluttertoast.showToast(msg: "Error. Please try again, or add a different name");
+                  }
+                }
               },
               child: Text('Proceed')
             ),
@@ -31,7 +53,7 @@ class AddPlayerBtn extends StatelessWidget {
                 Fluttertoast.showToast(msg: "Cancelled");
                 Navigator.pop(context);
               },
-              child: Text('Cancel')
+              child: const Text('Cancel')
             ),
           ],
           );
