7fc29c12b7ddac3b718f367e6c77f5bddfe266c7
CdeJohane <j.r.t.johane@gmail.com>
2024-09-09 19:43:19 +0200

    Added fixtures request controller

---
+++ lib/controller/fixtures_request.dart
@@ -0,0 +1,9 @@
+import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+
+Future<List<dynamic>> getAllPlayers() async{
+  var result = await http.get(Uri.parse(dotenv.env['BACKEND_SITE_GET_FIXTURES']!));
+  var resultArray = jsonDecode(result.body);
+  return resultArray;
+}
