52e33de584e253dcc184095f780ec79986a56b60
CdeJohane <j.r.t.johane@gmail.com>
2024-09-16 19:00:20 +0200

    Results Structure

--- lib/pages/results.dart
+++ lib/pages/results.dart
@@ -1,12 +1,56 @@
 import 'package:flutter/material.dart';
 
-class ResultsPage extends StatelessWidget {
+class ResultsPage extends StatefulWidget {
   const ResultsPage({super.key});
 
   @override
+  State<ResultsPage> createState() => _ResultsPageState();
+}
+
+class _ResultsPageState extends State<ResultsPage> {
+  List<int> items = List<int>.generate(38, (index) => index + 1);
+  int? match_week = 1;
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-        child: Text('Hello From results'),
+    return Column(
+      children: [
+        // One to select the Gameweek
+        Expanded(
+          flex: 1,
+          child: Container(
+            child: DropdownButton<int>(
+              isExpanded: true,
+              style: TextStyle(color: Colors.white),
+              items: items.map<DropdownMenuItem<int>>((int value) {
+              return DropdownMenuItem<int>(
+                    value: value,
+                    child: Text(value.toString()),
+                  );
+                }).toList(),
+                onChanged: (value) {
+                  setState(() {
+                    match_week = value;
+                  });
+                },
+            ),
+          ),
+        ),
+        // Another to show the results of the Matchweek
+        Expanded(
+          flex: 9,
+          child: Center(
+            child: Column(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Text('MatchDay $match_week', style: const TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.w700),),
+                ),
+                Expanded(flex: 8, child: Center(child: Text('My Text')))
+              ],
+            ),
+          ),
+        )
+      ],
     );
   }
 }
